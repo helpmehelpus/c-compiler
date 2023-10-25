0e2dd86cf1753983abbf3dad5686e6633fe4d0b1
helpmehelpus <adrianortorres@gmail.com>
2023-10-25 21:00:37 +1030

    reorder expressions

--- PeachCompiler/test.c
+++ PeachCompiler/test.c
@@ -1,10 +1,29 @@
-union abc
-{
-    int x;
-} rato;
-struct dog;
+struct book {
+    char name[30];
+};
 
-int main(int x)
+int test(char* fmt)
 {
-    x = (int) 30;
+    return 1;
 }
+
+struct book book;
+
+int main() {
+    struct book* books;
+    return test(65, books[0].name, 1000);
+}
+
+/*
+ * root exp
+ *
+ * books[0] is left expression
+ *
+ * .name is right expression -> name is left node, right node is 1000; operator is . (dot)
+ *
+ * Initially read as books[0].(name, 1000). We need to reorder the nodes
+ *
+ * After call to parser_node_move_right_left_to_left, name will now be in the left-hand side: (books[0].name, 1000)
+ *
+ *
+ */
